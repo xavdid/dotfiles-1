9950ba88c6f578b88d7bb4b1082f76a72781cbd0
David Brownman <beamneocube@gmail.com>
2019-03-08 14:51:43 -0500

    improve new_game and add python bin to path

--- python/path.zsh
+++ python/path.zsh
@@ -3,3 +3,5 @@ export WORKON_HOME=~/.virtualenv
 if [ -f /usr/local/bin/virtualenvwrapper.sh ];then
     source /usr/local/bin/virtualenvwrapper.sh
 fi
+
+export PATH="/Users/david/Library/Python/2.7/bin:$PATH"
