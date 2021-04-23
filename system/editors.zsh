16c00dced00962b924de9f067c79dc789386b4de
David Brownman <beamneocube@gmail.com>
2021-04-23 00:13:20 -0500

    remove subl and add support for opening dirs with .python-version

--- system/editors.zsh
+++ system/editors.zsh
@@ -3,4 +3,7 @@ alias zshconfig="$EDITOR ~/.zshrc"
 alias gitconfig="$EDITOR ~/.gitconfig"
 alias envedit="$EDITOR ~/.env"
 alias dotedit="$EDITOR $DOTFILES"
-alias c="$EDITOR ."
+# special case for code, which runs a line of python
+# if there's a python-version, it fails unless it's installed
+# needs to match the default python version
+alias c="PYENV_VERSION=3.9.0 code ."
